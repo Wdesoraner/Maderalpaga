ee0026d1fd9467832a326ba8fca5d72278036567
Lucas <lucasborel21@gmail.com>
2019-11-10 11:42:15 +0100

    [ADD] Design formulaire devis
    Ajout de fichiers html, css et js pour le formulaire
    WIP : Informations client

---
+++ Madera/Madera/www/scripts/formQuotation.js
@@ -0,0 +1,41 @@
+﻿$(document).ready(function () {
+
+    var navListItems = $('ul.setup-panel li a'),
+        allWells = $('.setup-content');
+
+    allWells.hide();
+
+    navListItems.click(function (e) {
+        e.preventDefault();
+        var $target = $($(this).attr('href')),
+            $item = $(this).closest('li');
+
+        if (!$item.hasClass('disabled')) {
+            navListItems.closest('li').removeClass('active');
+            $item.addClass('active');
+            allWells.hide();
+            $target.show();
+        }
+    });
+
+    $('ul.setup-panel li.active a').trigger('click');
+
+    $('#activate-quotationInfos').on('click', function (e) {
+        $('ul.setup-panel li:eq(1)').removeClass('disabled');
+        $('ul.setup-panel li a[href="#quotationInfos"]').trigger('click');
+        $(this).remove();
+    });
+
+    $('#activate-collection').on('click', function (e) {
+        $('ul.setup-panel li:eq(1)').removeClass('disabled');
+        $('ul.setup-panel li a[href="#collection"]').trigger('click');
+        $(this).remove();
+    });
+
+    $('#activate-summary').on('click', function (e) {
+        $('ul.setup-panel li:eq(1)').removeClass('disabled');
+        $('ul.setup-panel li a[href="#summary"]').trigger('click');
+        $(this).remove();
+    });
+});
+
