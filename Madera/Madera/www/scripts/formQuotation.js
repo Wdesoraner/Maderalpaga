d4cb21c66a69d1db239b658dd9aed19b70c177a4
Borel <lucas.borel@groupe-elabor.fr>
2019-11-14 14:35:46 +0100

    Progres wip design : js repond mieux

--- Madera/Madera/www/scripts/formQuotation.js
+++ Madera/Madera/www/scripts/formQuotation.js
@@ -1,12 +1,41 @@
-﻿$(document).ready(function () {
-    $("#btnclic").click(function () {
-        $('.nav-tabs a[href="#step2"]').tab('show');
-    });
-    
-});
+﻿//$(document).ready(function () {
+//    $("#btnclic").click(function () {
+//        $('.nav-tabs a[href="#step2"]').tab('show');
+//    });
+
+//});
 
 
 
-function activaTab(tab) {
-    $('.nav-tabs a[href="#' + tab + '"]').tab('show');
-};
+//function activaTab(tab) {
+//    $('.nav-tabs a[href="#' + tab + '"]').tab('show');
+//};
+
+
+
+$('.next').click(function () {
+
+    var nextId = $(this).parents('.tab-pane').next().attr("id");
+    $('[href=#' + nextId + ']').tab('show');
+    return false;
+
+})
+
+$('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+
+    //update progress
+    var step = $(e.target).data('step');
+    var percent = (parseInt(step) / 5) * 100;
+
+    $('.progress-bar').css({ width: percent + '%' });
+    $('.progress-bar').text("Step " + step + " of 5");
+
+    //e.relatedTarget // previous tab
+
+})
+
+$('.first').click(function () {
+
+    $('#myWizard a:first').tab('show')
+
+})
