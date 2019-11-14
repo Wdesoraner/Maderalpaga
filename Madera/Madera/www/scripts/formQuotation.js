0fcac306e720182f04da2236c97485fe39327bec
Lucas <lucasborel21@gmail.com>
2019-11-14 07:29:50 +0100

    WIP form devis

--- Madera/Madera/www/scripts/formQuotation.js
+++ Madera/Madera/www/scripts/formQuotation.js
@@ -1,41 +1,12 @@
 ﻿$(document).ready(function () {
-
-    var navListItems = $('ul.setup-panel li a'),
-        allWells = $('.setup-content');
-
-    allWells.hide();
-
-    navListItems.click(function (e) {
-        e.preventDefault();
-        var $target = $($(this).attr('href')),
-            $item = $(this).closest('li');
-
-        if (!$item.hasClass('disabled')) {
-            navListItems.closest('li').removeClass('active');
-            $item.addClass('active');
-            allWells.hide();
-            $target.show();
-        }
+    $("#btnclic").click(function () {
+        $('.nav-tabs a[href="#step2"]').tab('show');
     });
-
-    $('ul.setup-panel li.active a').trigger('click');
-
-    $('#activate-quotationInfos').on('click', function (e) {
-        $('ul.setup-panel li:eq(1)').removeClass('disabled');
-        $('ul.setup-panel li a[href="#quotationInfos"]').trigger('click');
-        $(this).remove();
-    });
-
-    $('#activate-collection').on('click', function (e) {
-        $('ul.setup-panel li:eq(1)').removeClass('disabled');
-        $('ul.setup-panel li a[href="#collection"]').trigger('click');
-        $(this).remove();
-    });
-
-    $('#activate-summary').on('click', function (e) {
-        $('ul.setup-panel li:eq(1)').removeClass('disabled');
-        $('ul.setup-panel li a[href="#summary"]').trigger('click');
-        $(this).remove();
-    });
+    
 });
 
+
+
+function activaTab(tab) {
+    $('.nav-tabs a[href="#' + tab + '"]').tab('show');
+};
