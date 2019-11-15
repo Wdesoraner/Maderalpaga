a4d90681c39ebe82f4a8162a6f977b377ac5d4a9
Borel <lucas.borel@groupe-elabor.fr>
2019-11-15 16:13:57 +0100

    js fonctionnel design et contenu wip

--- Madera/Madera/www/scripts/formQuotation.js
+++ Madera/Madera/www/scripts/formQuotation.js
@@ -1,9 +1,7 @@
-﻿//$(document).ready(function () {
-//    $("#btnclic").click(function () {
-//        $('.nav-tabs a[href="#step2"]').tab('show');
-//    });
+﻿$(document).ready(function () {
+    $('[href="#step1"]').tab('show');
 
-//});
+});
 
 
 
@@ -26,17 +24,17 @@ $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
 
     //update progress
     var step = $(e.target).data('step');
-    var percent = (parseInt(step) / 5) * 100;
+    var percent = (parseInt(step) / 4) * 100;
 
     $('.progress-bar').css({ width: percent + '%' });
-    $('.progress-bar').text("Step " + step + " of 5");
+    $('.progress-bar').text("Étape " + step + " sur 4");
 
     //e.relatedTarget // previous tab
 
 })
 
-$('.first').click(function () {
+//$('.first').click(function () {
 
-    $('#myWizard a:first').tab('show')
+//    $('#myWizard a:first').tab('show')
 
-})
+//})
