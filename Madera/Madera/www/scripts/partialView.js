e879b4999e38a2fd9aa832cd10dcd7db90ec1e87
Jules <julesrakic21@hotmail.fr>
2019-11-17 11:18:31 +0100

    vue partielle et vue client
    Conception de la vue partielle avec et ans Handlebars
    Conception vue gestion client

--- Madera/Madera/www/scripts/partialView.js
+++ Madera/Madera/www/scripts/partialView.js
@@ -1,1 +1,20 @@
 ﻿
+$(document).ready(function () {
+
+    initElement();
+});
+
+function initElement() {
+    var p = document.getElementById("home");
+    p.onclick = showAlert;
+
+    //const el = $('#content');
+    //const test = Handlebars.compile($('#contenu').html());
+    //const html = test();
+    //el.html(html);
+};
+
+function showAlert() {
+    $('#content').load("authentication.html");
+}
+
