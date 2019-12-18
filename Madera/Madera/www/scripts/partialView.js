6455aad6834b2ec8da5af9202e2e2bf850ef6e21
Darkhelloide <antoine.morin1@uqac.ca>
2019-12-18 11:44:00 +0100

    [UPD] Fin SPA + modif CSS

--- Madera/Madera/www/scripts/partialView.js
+++ Madera/Madera/www/scripts/partialView.js
@@ -8,8 +8,15 @@ function initElement() {
     var home = document.getElementById("home");
     home.onclick = function () { document.getElementById("content").innerHTML = '<object style="width:100%;height:95vh;" type="text/html" data="homeSlideshow.html" ></object>' };
 
-    var client = document.getElementById("home");
+    var client = document.getElementById("customer");
     client.onclick = function () { document.getElementById("content").innerHTML = '<object style="width:100%;height:95vh;" type="text/html" data="client.html" ></object>' };
+
+    var createQuote = document.getElementById("createQuote");
+    createQuote.onclick = function () { document.getElementById("content").innerHTML = '<object style="width:100%;height:95vh;" type="text/html" data="listQuotation.html" ></object>' };
+
+    var projects = document.getElementById("projects");
+    projects.onclick = function () { document.getElementById("content").innerHTML = '<object style="width:100%;height:95vh;" type="text/html" data="listProject.html" ></object>' };
+
 };
 
 
