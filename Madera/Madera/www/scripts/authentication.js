54de5c689e37c90ab5c5ae3a8ab7a9d05b675125
Jules <julesrakic21@hotmail.fr>
2019-12-19 15:50:08 +0100

    [UPD] top bar
    Ajout top bar

---
+++ Madera/Madera/www/scripts/authentication.js
@@ -0,0 +1,27 @@
+﻿
+
+$(document).ready(function () {
+    document.getElementById("connexion").onclick = connexion;
+});
+
+
+function connexion() {
+    
+    if (checkRequiredInput()) {
+        let login = document.getElementById("login").value
+        let mdp = document.getElementById("password").value  
+        console.log(sha256(mdp));
+    }
+}
+
+function checkRequiredInput() {
+    var inputList = document.getElementsByTagName('input');
+    for (var i = 0; i < inputList.length; i++) {
+        if (inputList[i].value === '' && inputList[i].hasAttribute('required')) {
+            alert('Certains champs requis sont incomplets!');
+            return false;
+        }
+    };
+    return true;
+
+}
