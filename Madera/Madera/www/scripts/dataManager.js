3571114cf061d278d47d7f68e8a62caf9476c00f
Jules <julesrakic21@hotmail.fr>
2019-12-30 11:39:05 +0100

    [ADD] dataManager
    Ajout d'une fonction de sérialisation de la base locale

---
+++ Madera/Madera/www/scripts/dataManager.js
@@ -0,0 +1,43 @@
+﻿function exportToJsonString(idbDatabase, cb) {
+    var exportObject = {};
+    if (idbDatabase.objectStoreNames.length === 0)
+        cb(null, JSON.stringify(exportObject));
+    else {
+        var transaction = idbDatabase.transaction(idbDatabase.objectStoreNames, "readonly");
+        transaction.onerror = function (event) {
+            cb(event, null);
+        };
+        Array.prototype.forEach.call(idbDatabase.objectStoreNames, function (storeName) {
+            var allObjects = [];
+            transaction.objectStore(storeName).openCursor().onsuccess = function (event) {
+                var cursor = event.target.result;
+                if (cursor) {
+                    allObjects.push(cursor.value);
+                    cursor.continue();
+                } else {
+                    exportObject[storeName] = allObjects;
+                    console.log(exportObject);
+                    if (idbDatabase.objectStoreNames.length === Object.keys(exportObject).length) {
+                        cb(null, JSON.stringify(exportObject));
+                    }
+                }
+            };
+        });
+    }
+}
+
+function exportData() {
+    let db;
+    let DBOpenRequest = window.indexedDB.open("maderaDB");
+
+    DBOpenRequest.onsuccess = function (event) {
+        db = event.target.result;
+        exportToJsonString(db, function (err, jsonString) {
+            if (err)
+                console.error(err);
+            else {
+                console.log("Exported as JSON: " + jsonString);
+            }
+        });
+    }
+}
